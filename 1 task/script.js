93da32d25f14db95c941cafebde0ecda7a952244
ellaelisimo <gabriela.barsukova@gmail.com>
2022-10-27 19:47:40 +0300

    nekovertuoja, bet visa kita padaryta

--- 1 task/script.js
+++ 1 task/script.js
@@ -9,3 +9,43 @@ Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
 //sukurti header ir footer
+
+const createheaderFooter = () => {
+  const header = document.createElement("header");
+  const footer = document.createElement("footer");
+
+  document.body.prepend(header);
+  document.body.append(footer);
+};
+
+createheaderFooter();
+
+document
+  .getElementById("submit-btn")
+  .addEventListener("click", (event, valNum) => {
+    event.preventDefault();
+
+    const tableElement = document.createElement("table");
+    const poundsElement = document.createElement("tr");
+    const gramsElement = document.createElement("tr");
+    const ouncesElement = document.createElement("tr");
+
+    poundsElement.innerText = "Pounds:";
+    gramsElement.innerText = "Grams:";
+    ouncesElement.innerText = "Ounces:";
+
+    tableElement.append(poundsElement, gramsElement, ouncesElement);
+    document.getElementById("output").append(tableElement);
+
+    const spanPoundsElement = document.createElement("span");
+    const spanGramsElement = document.createElement("span");
+    const spanOuncesElement = document.createElement("span");
+
+    poundsElement.append(spanPoundsElement);
+    gramsElement.append(spanGramsElement);
+    ouncesElement.append(spanOuncesElement);
+
+    spanPoundsElement.innerHTML = parseFloat(valNum * 2.2046);
+    spanGramsElement.innerText = parseFloat(valNum / 0.001);
+    spanOuncesElement.innerText = parseFloat(valNum * 35.274);
+  });
