6e043225461cda1c071956967ac742cce6670fa8
ellaelisimo <gabriela.barsukova@gmail.com>
2022-10-28 19:31:31 +0300

    task 6 completed

---
+++ 6 task/script.js
@@ -0,0 +1,19 @@
+const users = [
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
+];
+
+function getUserAverageAge(arr) {
+  return users.reduce((acc, { age }) => acc + age, 0) / arr.length;
+}
+console.log(getUserAverageAge(users));
+
+const getUsersNames = (data) => data.flat().map(({ name }) => name);
+console.log(getUsersNames(users));
