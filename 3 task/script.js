1092815fe5b19008554a5430e6cf99b08d24a0aa
ellaelisimo <gabriela.barsukova@gmail.com>
2022-10-30 21:14:57 +0200

    not finished 3 task, couldn't get user

--- 3 task/script.js
+++ 3 task/script.js
@@ -1,59 +1,45 @@
-/* ------------------------------ TASK 3 -----------------------------------
-Parašykite JS kodą, kuris leis vartotojui paspaudus ant mygtuko "Show users"
-pamatyti vartotojus iš Github API (endpoint'as pateiktas žemiau).
-
-Paspaudus mygtuką "Show users":
-1. Informacija atvaizdavima <div id="output"></div> bloke
-1.1. Informacija, kuri pateikiama: "login" ir "avatar_url" reikšmės (kortelėje)
-2. Žinutė "Press "Show Users" button to see users" turi išnykti;
-
-Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi 
-turėti bent minimalų stilių ir būti responsive;
--------------------------------------------------------------------------- */
+// taip ir nesugebėjua išsiaiškint, kame problema ir kaip ją spręst...
 
 const ENDPOINT = "https://api.github.com/users";
 
-// document.querySelector("#btn-container").addEventListener("click", () => {
+const createheaderFooter = () => {
+  const header = document.createElement("header");
+  const footer = document.createElement("footer");
 
-// //   const tableBodyElement = document.createElement("tbody");
-// //   const trElement = document.createElement("tr");
-// //   const tdLoginElement = document.createElement("td");
-// //   const tdAvatarElement = document.createElement("td");
+  document.body.prepend(header);
+  document.body.append(footer);
+};
 
-// //   trElement.append(tdLoginElement, tdAvatarElement);
-// //   tableBodyElement.append(trElement);
-// //   document.querySelector("#output").append(tableBodyElement);
-// });
-
-document.querySelector("#btn").addEventListener("click", () => {
-  document.querySelector("#message").style.display = "none";
-  document.querySelector("#output").style.backgroundColor = "#222";
-
-  const renderUserCard = (user) => {
-    const avatar = document.createElement("img");
-    const login = document.createElement("h4");
-    const card = document.createElement("div");
-
-    // avatar.src = user.avatar_url;
-    // login.innerText = `${user.login} `;
-
-    card.append(avatar, login);
-    document.querySelector("#output").append(card);
-  };
-  renderUserCard();
-
-  const populateCard = () => {};
-});
+createheaderFooter();
 
 const getUsers = async () => {
   try {
     const response = await fetch(ENDPOINT);
-    const users = await response.json();
-
-    return users;
+    if (response.ok) {
+      const data = await response.json();
+      return data.users;
+    }
   } catch (error) {
     console.error(error);
   }
 };
 
-console.log(getUsers());
+const showUsers = async (user) => {
+  await getUsers();
+  const userAvatar = document.createElement("img");
+  const login = document.createElement("h4");
+  const card = document.querySelector("div");
+
+  userAvatar.src = user.avatar_url;
+  login = user.login;
+
+  document.querySelector("#output").append(card);
+  card.append(userAvatar, login);
+};
+
+document.querySelector("#btn").addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  document.querySelector("#message").style.display = "none";
+  document.querySelector("#output").style.backgroundColor = "#222";
+});
